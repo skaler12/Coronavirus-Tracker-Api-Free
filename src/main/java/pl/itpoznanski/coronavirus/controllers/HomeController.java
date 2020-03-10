6c4dbe33bc6f0782e89cbffae86f8c5ef9049463
skaler12 <gpoznanski98@gmail.com>
2020-03-11 00:02:16 +0100

    initial commit

---
+++ src/main/java/pl/itpoznanski/coronavirus/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package pl.itpoznanski.coronavirus.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.itpoznanski.coronavirus.models.LocationStats;
+import pl.itpoznanski.coronavirus.services.CoronaVirusDataService;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+    @GetMapping("/")
+    public  String home(Model model){
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPreDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+        return "home";
+    }
+}
---
+++ src/main/java/pl/itpoznanski/coronavirus/models/LocationStats.java
@@ -0,0 +1,50 @@
+package pl.itpoznanski.coronavirus.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int diffFromPreDay;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    public int getDiffFromPreDay() {
+        return diffFromPreDay;
+    }
+
+    public void setDiffFromPreDay(int diffFromPreDay) {
+        this.diffFromPreDay = diffFromPreDay;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+}
